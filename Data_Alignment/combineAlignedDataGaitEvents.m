b267ee2de297382ac978c783cc4fa066c7040395
Kenneth Louie <klouie@Kenneths-iMac.ucsf.edu>
2021-02-18 13:15:30 -0800

    New function to combine aligned data with gait event csv file.

---
+++ Data_Alignment/combineAlignedDataGaitEvents.m
@@ -0,0 +1,20 @@
+function combineAlignedDataGaitEvents()
+% load Aligned data mat file
+[filename_aligned_data,path] = uigetfile('*.mat');
+load(fullfile(path,filename_aligned_data));
+
+% load Gait event file
+[filename_gait_events,path2] = uigetfile('*.csv');
+gait_events = readtable(fullfile(path2,filename_gait_events));
+
+gait_events_aligned = gait_events;
+for i = 1:length(gait_events.Properties.VariableNames)
+    gait_events_aligned.(gait_events.Properties.VariableNames{i}) = ...
+        gait_events_aligned.(gait_events.Properties.VariableNames{i}) - aligned_data.alignment_times(5) + aligned_data.pre_align_time;
+end
+
+aligned_data.gait_events = gait_events_aligned;
+
+savename = strrep(filename_aligned_data,'.mat','_w_Gait_Events.mat');
+save(fullfile(path,savename),'aligned_data');
+end
