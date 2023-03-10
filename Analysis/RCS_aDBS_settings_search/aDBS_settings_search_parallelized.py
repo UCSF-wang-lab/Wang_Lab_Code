Updated Bayesian optimization and grid search code. Fixed a accuracy calculation bug.

Analysis/RCS_aDBS_settings_search/aDBS_settings_search_parallelized.py
@@ -370,7 +370,7 @@ def main(n_power_bands: int = 1,save_path: str = os.getcwd()):
         # gp_options = {'nu':0.5,'length_scale':np.array([1e-2,2.5]),'length_scale_bounds':"fixed"}
         # gp_options = {'nu':1.5,'length_scale':np.array([1,2.5]),'length_scale_bounds':"fixed"}
         n_itr = 1000
-        gp_options = {"nu":1.5,"length_scale":np.array([1,2.5]),"length_scale_bounds":"fixed"}
+        gp_options = {"nu":0.5,"length_scale":np.array([1,2.5]),"length_scale_bounds":"fixed"}
 
         bo_option_list = [None] * 4
 
@@ -387,9 +387,10 @@ def main(n_power_bands: int = 1,save_path: str = os.getcwd()):
             bo_options["save_name_param_combos"] = save_name_param_combos.replace("full_spec","Bayes_Opt_key"+str(i))
             bo_option_list[i] = bo_options
 
-        runSearch(bo.runBO,bo_option_list[0])
-        # with processing_pool as pool:
-        #     pool.map(runSearch,bo_option_list)
+        # runSearch(bo_option_list[0])
+
+        with processing_pool as pool:
+            pool.map(runSearch,bo_option_list)
 
         # End run time
         toc = time.perf_counter()
@@ -411,9 +412,12 @@ def main(n_power_bands: int = 1,save_path: str = os.getcwd()):
         with processing_pool as pool:
             pool.map(runSearch,grid_search_option_list)
 
+        # End run time
+        toc = time.perf_counter()
+        print(f"Code ran in {toc-tic:0.4f} seconds")
 
     # Close the processing pool
-    # processing_pool.Close()
+    processing_pool.Close()
 
 
 

Analysis/RCS_aDBS_settings_search/aDBS_threshold_search.py
@@ -196,18 +196,19 @@ def save_multiple_csv(data,n_entries,base_path: str = os.getcwd()):
 def runSearch(search_arguments):
     if "bayesOpt" in search_arguments["method_function"].__module__:
         bo_options = {dict_key: search_arguments[dict_key] for dict_key in ("n_itr","gp_params","min_fun","param_combos","key","search_type")}
-        out_DF,raw_X,raw_Y = search_arguments["method_function"](search_arguments["optimizing_function"],search_arguments["data"],search_arguments["gait_events"],bo_options)
+        out_DF,X,Y_weighted,Y_dst,Y_raw = search_arguments["method_function"](search_arguments["optimizing_function"],search_arguments["data"],search_arguments["gait_events"],bo_options)
 
         # Save data to save progress
         out_DF.to_csv(search_arguments["save_name_results"],index = False)
 
         # Generate dataframes of the Gaussian process and acquisition function so I can plot in MATLAB
-        GP_itr,AF_itr = bo.BOItrDF(raw_X,raw_Y,n_initial_samples=out_DF.shape[0]-search_arguments["n_itr"],gp_params = search_arguments["gp_options"],itr_spacing = [100,50,10,1], itr_reps = [8,2,7,30])
+        # GP_itr,AF_itr = bo.BOItrDF(X,Y_weighted,n_initial_samples=out_DF.shape[0]-search_arguments["n_itr"],gp_params = search_arguments["gp_params"],itr_spacing = [100,50,10,1], itr_reps = [8,2,7,30])
+        GP_itr,AF_itr = bo.BOItrDF(X,Y_weighted,search_arguments["param_combos"],n_initial_samples=out_DF.shape[0]-search_arguments["n_itr"],gp_params = search_arguments["gp_params"])
 
         # Save the iteration outputs and parameter combos that were evaluated
         np.savetxt(search_arguments["save_name_param_combos"],search_arguments["param_combos"],delimiter = ",")
-        save_multiple_csv(GP_itr,n_entries = 50,base_path = search_arguments["save_name_GP_itr"])
-        save_multiple_csv(AF_itr,n_entries = 50,base_path = search_arguments["save_name_AF_itr"])
+        save_multiple_csv(GP_itr,n_entries = 100,base_path = search_arguments["save_name_GP_itr"])
+        save_multiple_csv(AF_itr,n_entries = 100,base_path = search_arguments["save_name_AF_itr"])
 
     elif "gridSearch" in search_arguments["method_function"].__module__:
         gs_options = {dict_key: search_arguments[dict_key] for dict_key in ("param_combos","key","search_type")}
@@ -280,7 +281,6 @@ def main(n_power_bands: int = 1,parallized_flag: bool = False,save_path: str = o
         # event_strings = []    # TODO add in options to specify gait phase
         # gp_options = {'nu':0.5,'length_scale':np.array([1e-2,2.5]),'length_scale_bounds':"fixed"}
         # gp_options = {'nu':1.5,'length_scale':np.array([1,2.5]),'length_scale_bounds':"fixed"}
-        n_itr = 1000
 
         if search_type == "all_combo":
             gp_options = {"nu":0.5,"length_scale":np.array([1,2.5]),"length_scale_bounds":"fixed"}
@@ -292,9 +292,8 @@ def main(n_power_bands: int = 1,parallized_flag: bool = False,save_path: str = o
 
         for i in range(len(search_keys)):
             # Set search parameters
-            bo_options = {"method_function":bo.runBO,"optimizing_function":ta.calcThresholdAccuracyDST,"gait_events":gait_events,"n_itr":n_itr,"gp_params":gp_options,"min_fun":False}
+            bo_options = {"method_function":bo.runBO,"optimizing_function":ta.calcThresholdAccuracyDST,"gait_events":gait_events,"gp_params":gp_options,"min_fun":False}
             bo_options["data"] = pb_DataFrame_dict[search_keys[i]]
-            bo_options["n_itr"] = n_itr
             bo_options["key"] = search_keys[i]
             bo_options["search_type"] = search_type
             bo_options["save_name_results"] = save_name_results.replace("full_spec","Bayes_Opt_"+search_keys[i])
@@ -321,6 +320,9 @@ def main(n_power_bands: int = 1,parallized_flag: bool = False,save_path: str = o
                 parameter_range = data_quantiles.reshape((-1,2))
                 param_combos = bo.createParamSpace(parameter_range,spacing_type,n_spacings=spacing)
                 bo_options["param_combos"] = param_combos
+
+            # Set the number of iterations to be 50% of the total number of parameter combos
+            bo_options["n_itr"] = round(param_combos.shape[0]*0.5)
 
             if parallized_flag == "True":
                 bo_option_list[i] = bo_options

Analysis/RCS_aDBS_settings_search/libs/bayesOpt/bayesOpt.py
@@ -540,7 +540,7 @@ def runBO(func_2_optimize, RCS_data: pd.DataFrame, event_timings:pd.DataFrame,bo
             pb_data = RCS_data.iloc[:,round(next_sample_point[0])].to_numpy()
             threshold_val = next_sample_point[1]
         
-        new_outcome,new_dst,new_full = func_2_optimize(RCS_time,pb_data,event_timings,threshold_val)
+        new_outcome,new_full,new_dst = func_2_optimize(RCS_time,pb_data,event_timings,threshold_val)
 
         # Update the list of evaluated sample points and outcomes
         if "search_type" in bo_options.keys():
@@ -572,7 +572,12 @@ def runBO(func_2_optimize, RCS_data: pd.DataFrame, event_timings:pd.DataFrame,bo
 
 
     # Create output
-    result_table = pd.DataFrame(np.hstack((X,Y,Y_dst,Y_full)),columns = ["Frequency Band Ind","Threshold","Weighted Accuracy","DST Accuracy","Full Accuracy"])
+    column_names = [None]*(n_param+3)
+    for i in range(n_param):
+        column_names[i] = "x%d" % (i+1)
+    column_names[n_param:n_param+3] = ["Weighted Accuracy","DST Accuracy","Full Accuracy"]
+
+    result_table = pd.DataFrame(np.hstack((X,Y,Y_dst,Y_full)),columns = column_names)
 
     # Return sampled values and outputs. 
     # Possibly add in the EI and GP output for each iteration as well.

Analysis/RCS_aDBS_settings_search/libs/optimizingFunctions/thresholdAccuracy.py
@@ -81,15 +81,15 @@ def calcThresholdAccuracyDST(RCS_time,RCS_data,event_timings,threshold_val,flip_
     # Determine how many nan's there are. These should not be counted in the accuracy calculations
     D = np.where(event_timings.iloc[:,0].isnull().values==True)[0]
     E = np.where(event_timings.iloc[:,1].isnull().values==True)[0]
-    n_nans = len(D|E)
+    n_nans = len(list(set().union(D,E)))
 
     # Determine how many double support times would not have been detected anyway
     event_not_detected = np.zeros((event_timings.shape[0],1))
     for j in range(event_timings.shape[0]):
-        if not np.isnan(event_timings.iloc[j,0]):
+        if not (np.isnan(event_timings.iloc[j,0])) | (np.isnan(event_timings.iloc[j,1])):
             F = RCS_time >= event_timings.iloc[j,0]
             G = RCS_time <= event_timings.iloc[j,1]
-            if not(sum(F&G) >= 1):
+            if sum(F&G) == 0:
                 event_not_detected[j] = 1
 
     full_thresh_accuracy = sum(correct_state)/len(correct_state)