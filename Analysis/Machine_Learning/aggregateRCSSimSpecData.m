fd0eab7c45ac5c5411215219155100c447bd9922
Kenneth Louie <klouie@Kenneths-MBP-2.ucsf.edu>
2023-12-08 15:24:22 -0800

    New python script to create csv files of every possible fft combo for an RCS recording. Added new matlab processing function to aggregate all the possible fft combos tables.

---
+++ Analysis/Machine_Learning/aggregateRCSSimSpecData.m
@@ -0,0 +1,188 @@
+function aggregateTables = aggregateRCSSimSpecData(patientFolder)
+patientFolder = '/Volumes/dwang3_shared/Patient Data/RC+S Data/gait_RCS_02';
+folderList = dir([patientFolder,'/*v*']);
+subjectID = patientFolder(find(patientFolder=='/',1,'last')+1:end);
+aggregateTables = [];
+
+% Go through all of the patient's folders
+for i = 1:length(folderList)
+    % Check to make sure the Rover folder is not included
+    if ~strcmp(folderList(i).name,'Rover')
+        fprintf('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n');
+        fprintf('Parsing folder: %s\n',folderList(i).name);
+        
+        % Grab all the spectrogram and mat files
+        spec_files = dir(fullfile(folderList(i).folder,folderList(i).name,'Data','Analysis Data','aDBS','*bitshift*'));
+        mat_files = dir(fullfile(folderList(i).folder,folderList(i).name,'Data','Aligned Data','*w_Gait_Events*'));
+        
+        if ~isempty(spec_files)
+            % Trim number of mat files to the most recent ones
+            mat_files = trimMatFiles(mat_files);
+            
+            % Loop through mat files and associated spec files and fill in a
+            % table
+            for j = 1:length(mat_files)
+                % Load in mat file
+                load(fullfile(folderList(1).folder,folderList(i).name,'Data/Aligned Data',mat_files{j}));
+                
+                % Check which spec files are associated with this file
+                specPatientState = sprintf('%s_stim_%s_med',aligned_data.stim_condition,aligned_data.med_condition);
+                specFileMatch = cellfun(@(x)contains(x,specPatientState,'IgnoreCase',true),{spec_files.name});
+                specFileNames = {spec_files(specFileMatch).name};
+                
+                % Used to keep track of spec params. Reduces additional
+                % computations
+                prev_fs = 0;
+                prev_nfft = 0;
+                prev_fft_int = 0;
+                prev_hemisphere = 0;
+                
+                % Create table of all spec files that are related to each
+                % other
+                for k = 1:length(specFileNames)
+                    fprintf('Reading file: %s\n',specFileNames{k});
+                    
+                    % Load in spectral data
+                    specData = readtable(fullfile(folderList(1).folder,folderList(i).name,'Data/Analysis Data/aDBS',specFileNames{k}));
+                    
+                    % Grab current spec parameters and hemisphere
+                    specParam = strsplit(specFileNames{k},'_');
+                    fs = round(str2double(specParam(find(cellfun(@(x) strcmp(x,'fs'),specParam))+1)));
+                    nfft = round(str2double(specParam(find(cellfun(@(x) strcmp(x,'nfft'),specParam))+1)));
+                    fft_int = round(str2double(specParam(find(cellfun(@(x) strcmp(x,'fftint'),specParam))+1)));
+                    overlap = round(str2double(specParam(find(cellfun(@(x) strcmp(x,'overlap'),specParam))+1)));
+                    bitshift = round(str2double(specParam(find(cellfun(@(x) strcmp(x,'fftbitshift'),specParam))+1)));
+                    
+                    if contains(specFileNames{k},'left','IgnoreCase',true)
+                        hemisphere = "left";
+                    else
+                        hemisphere = "right";
+                    end
+                    
+                    % Check to see if this calculation was done previously
+                    % but for a different bitshift or hemisphere. If it
+                    % wasn't, calculate it for the new fft settings.
+%                     sameSpecParams = (fs ~= prev_fs && nfft ~= prev_nfft && fft_int ~= prev_fft_int && strcmp(hemisphere,prev_hemisphere));
+                    sameSpecParams = 0;
+                    if ~sameSpecParams
+                        state = zeros(height(specData),1);
+                        
+                        % Update values
+                        prev_fs = fs;
+                        prev_nfft = nfft;
+                        prev_fft_int = fft_int;
+                        prev_hemisphere = hemisphere;
+                        
+                        % New parameters, calculate the correct states base
+                        % on gait events.
+                        sortedGaitEvents = sortGaitEvents(aligned_data.gait_events,'RTO');
+                        for m = 1:height(specData)
+                            % Specific for swing phase. Future will change
+                            % to make this generic.
+                            if strcmp(hemisphere,'left')
+                                toeOff = (specData.time(m)>=sortedGaitEvents.RTO) & (specData.time(m)<=sortedGaitEvents.RHS);
+                                if sum(toeOff) == 1
+                                    state(m) = 1;
+                                end
+                            else
+                                toeOff = (specData.time(m)>=sortedGaitEvents.LTO) & (specData.time(m)<=sortedGaitEvents.LHS);
+                                if sum(toeOff) == 1
+                                    state(m) = 1;
+                                end
+                            end
+                        end
+                    end
+                    
+                    % Create table with states and values. Always
+                    % recalculate as verification on the RCS recording
+                    % contacts are the same. 
+                    includeColumns = cellfun(@isempty,cellfun(@(x)regexp(x,'time|_[0-3]_'),specData.Properties.VariableNames,'UniformOutput',false));
+                    currTable = specData(:,includeColumns);
+                    
+                    % Other columns
+                    subjectIDVec = repmat({subjectID},height(currTable),1);
+                    
+                    switch subjectID
+                        case 'gait_RCS_02'
+                            if contains(folderList(i).name,'v4')
+                                visitNameVec = repmat({'preprogrammingUnilateral'},height(currTable),1);
+                            elseif contains(folderList(i).name,'v7')
+                                visitNameVec = repmat({'preprogrammingBilateral'},height(currTable),1);
+                            elseif contains(folderList(i).name,'v5')
+                                visitNameVec = repmat({'dbsOptUnilateral'},height(currTable),1);
+                            elseif contains(folderList(i).name,'v8')
+                                visitNameVec = repmat({'dbsOptBilateral'},height(currTable),1);
+                            end
+                        case 'gait_RCS_05'
+                        otherwise
+                    end
+                    
+                    medStateVec = repmat({aligned_data.med_condition},height(currTable),1);
+                    stimStateVec = repmat({aligned_data.stim_condition},height(currTable),1);
+                    hemisphereVec = repmat({hemisphere},height(currTable),1);
+                    fsVec = repmat(fs,height(currTable),1);
+                    nfftVec = repmat(nfft,height(currTable),1);
+                    overlapVec = repmat(overlap,height(currTable),1);
+                    fftIntVec = repmat(fft_int,height(currTable),1);
+                    bitshiftVec = repmat(bitshift,height(currTable),1);
+                    extraTable = table(subjectIDVec,visitNameVec,medStateVec,stimStateVec,hemisphereVec,fsVec,nfftVec,overlapVec,fftIntVec,bitshiftVec,state,...
+                        'VariableNames',{'SubjectID','VisitName','MedState','StimState','Side','FS','NFFT','OverlapPercent','FFTInterval','Bitshift','Class'});
+                    outTable = [extraTable,currTable];
+                    
+                    aggregateTableName = sprintf('%s_AggregateSpecData_fs%d_nfft%d',subjectID,fs,nfft);
+                    
+                    if isempty(aggregateTables) || sum(cellfun(@(x) strcmp(x,aggregateTableName),fields(aggregateTables))) == 0
+                        aggregateTables.(aggregateTableName) = outTable;
+                    elseif sum(cellfun(@(x) strcmp(x,aggregateTableName),fields(aggregateTables))) == 1
+                        aggregateTables.(aggregateTableName) = [aggregateTables.(aggregateTableName);outTable];
+                    end
+                end
+            end
+        end
+        fprintf('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n');
+    end
+end
+end
+
+function trimmedFiles = trimMatFiles(mat_files)
+trimmedFiles = [];
+
+off_stim_off_med_files = matchFiles(mat_files,'off_stim_off_meds');
+off_stim_on_med_files = matchFiles(mat_files,'off_stim_on_meds');
+on_stim_off_med_files = matchFiles(mat_files,'on_stim_off_meds');
+on_stim_on_med_files = matchFiles(mat_files,'on_stim_on_meds');
+
+trimmedFiles = [off_stim_off_med_files;off_stim_on_med_files;on_stim_off_med_files';on_stim_on_med_files'];
+end
+
+function matchedFiles = matchFiles(file_names,name2match)
+matchedFiles = [];
+
+fileMatch = cellfun(@(x)contains(x,name2match,'IgnoreCase',true),{file_names.name});
+fileMatchInd = find(fileMatch);
+
+% Check if there are multiple trials for a single med and stim state
+if ~isempty(fileMatchInd)
+    fileNames = {file_names(fileMatchInd).name};
+    multipleTrials = sum(cellfun(@(x) contains(x,'Trial2','IgnoreCase',true),fileNames));
+    
+    if multipleTrials>0
+        for i = 1:2
+            % Find all files for Trial#i
+            trialMatch = cellfun(@(x) contains(x,sprintf('Trial%d',i),'IgnoreCase',true),fileNames);
+            trialFileInd = find(trialMatch);
+%             trialFileNames = fileNames(trialFileInd);
+            
+            % Determine most recent file
+            fileDates = cellfun(@datetime,{file_names(fileMatchInd(trialFileInd)).date});
+            [~,mostRecentFileInd] = max(fileDates);
+            matchedFiles{end+1} = file_names(trialFileInd(mostRecentFileInd)).name;
+        end
+    else
+        % Determine most recent file
+        fileDates = cellfun(@datetime,{file_names(fileMatchInd).date});
+        [~,mostRecentFileInd] = max(fileDates);
+        matchedFiles = {file_names(fileMatchInd(mostRecentFileInd)).name};
+    end
+end
+end
--- Analysis/RCS_Power_Sim/calculateRCSPower.py
+++ Analysis/RCS_Power_Sim/calculateRCSPower.py
@@ -24,6 +24,7 @@ import sys
 import os
 import numpy as np
 import pandas as pd
+import itertools
 from libs.rcssim import rcs_sim as rcs # Custom module. Grab latest version from github
 
 """
@@ -40,16 +41,34 @@ def loadData(file_path: str,type: str = "data") -> pd.DataFrame:
         raw_input = pd.read_csv(file_path,dtype = types_dict)
         formatted_input = raw_input.copy()
         formatted_input.amp_gains[0] = np.asarray(formatted_input.amp_gains[0].split('|'),dtype = int)
-        formatted_input.power_bands[0] = np.asarray(formatted_input.power_bands[0].split('|'),dtype = float)
-        formatted_input.power_bands[0] = formatted_input.power_bands[0].reshape(int(len(formatted_input.power_bands[0])/2),2)
+        if not pd.isnull(formatted_input.power_bands[0]):
+            formatted_input.power_bands[0] = np.asarray(formatted_input.power_bands[0].split('|'),dtype = float)
+            formatted_input.power_bands[0] = formatted_input.power_bands[0].reshape(int(len(formatted_input.power_bands[0])/2),2)
         return formatted_input
+    elif type == "gait_events":
+        gait_events = pd.read_csv(file_path)
+        return gait_events
     else:
         sys.exit('Data type flag not supported.')
 
 
-def calcRCSFFT(rcs_data: pd.DataFrame,rcs_settings: pd.DataFrame,pb: bool=False) -> pd.DataFrame:
+def calcRCSFFT(rcs_data: pd.DataFrame,rcs_settings: pd.DataFrame,pb: bool=False, fs = None, nfft = None, fft_int = None, fft_bitshift = None) -> pd.DataFrame:
+
+    if fs is None:
+        fs = rcs_settings.fs_td[0]
+
+    if nfft is None:
+        nfft = rcs_settings.FFT_size[0]
+
+    if fft_int is None:
+        fft_int = rcs_settings.FFT_interval[0]
+
+    if fft_bitshift is None:
+        fft_bitshift = rcs_settings.FFT_bitshift[0]
+
+
     # grab hann window values used by the RCS device
-    hann_win = rcs.create_hann_window(rcs_settings.FFT_size[0], percent=100)
+    hann_win = rcs.create_hann_window(nfft, percent=100)
 
     if pb:
         rcs_data_sim = rcs_data[['time','key0','key1','key2','key3']].copy()
@@ -69,25 +88,25 @@ def calcRCSFFT(rcs_data: pd.DataFrame,rcs_settings: pd.DataFrame,pb: bool=False)
             data_rcs_unit = rcs.transform_mv_to_rcs(rcs_data_sim[channel].values,rcs_settings.amp_gains[0][count])
             data_rcs_fft, time_pb = rcs.td_to_fft(data_rcs_unit,
                                                 rcs_data_sim['time'].values,
-                                                rcs_settings.fs_td[0],    # sample rate of the neural data
-                                                rcs_settings.FFT_size[0], # fft size set on the RCS
-                                                rcs_settings.FFT_interval[0], # update interval set on the RCS
+                                                fs,    # sample rate of the neural data
+                                                nfft, # fft size set on the RCS
+                                                fft_int, # update interval set on the RCS
                                                 hann_win,
                                                 output_in_mv = False)
             
             data_rcs_pb = rcs.fft_to_pb(data_rcs_fft,
-                                        rcs_settings.fs_td[0],
-                                        rcs_settings.FFT_size[0],
-                                        rcs_settings.FFT_bitshift[0],
+                                        fs,
+                                        nfft,
+                                        fft_bitshift,
                                         band_edges_hz = rcs_settings.power_bands[0][count*2,],
                                         input_is_mv = False)
             pb_sample_mask = np.isin(rcs_data_sim.time,time_pb)
             rcs_data_sim.loc[pb_sample_mask,'pb1_'+channel] = data_rcs_pb
 
             data_rcs_pb = rcs.fft_to_pb(data_rcs_fft,
-                                        rcs_settings.fs_td[0],
-                                        rcs_settings.FFT_size[0],
-                                        rcs_settings.FFT_bitshift[0],
+                                        fs,
+                                        nfft,
+                                        fft_bitshift,
                                         band_edges_hz = rcs_settings.power_bands[0][count*2+1,],
                                         input_is_mv = False)
             pb_sample_mask = np.isin(rcs_data_sim.time,time_pb)
@@ -102,19 +121,19 @@ def calcRCSFFT(rcs_data: pd.DataFrame,rcs_settings: pd.DataFrame,pb: bool=False)
             data_rcs_unit = rcs.transform_mv_to_rcs(rcs_data[channel].values,rcs_settings.amp_gains[0][count])
             data_rcs_fft, time_pb = rcs.td_to_fft(data_rcs_unit,
                                                 rcs_data['time'].values,
-                                                rcs_settings.fs_td[0],    # sample rate of the neural data
-                                                rcs_settings.FFT_size[0], # fft size set on the RCS
-                                                rcs_settings.FFT_interval[0], # update interval set on the RCS
+                                                fs,    # sample rate of the neural data
+                                                nfft, # fft size set on the RCS
+                                                fft_int, # update interval set on the RCS
                                                 hann_win,
                                                 output_in_mv = False)
             
             data_rcs_power = rcs.fft_to_pb(data_rcs_fft,
-                                        rcs_settings.fs_td[0],
-                                        rcs_settings.FFT_size[0],
-                                        rcs_settings.FFT_bitshift[0],
+                                        fs,
+                                        nfft,
+                                        fft_bitshift,
                                         input_is_mv = False)
             
-            key_freq_names = generateKeyFreqNames(rcs_settings.fs_td[0],rcs_settings.FFT_size[0],channel)
+            key_freq_names = generateKeyFreqNames(fs,nfft,channel)
             curr_df = pd.DataFrame(data_rcs_power,columns = key_freq_names)
             rcs_data_sim = pd.concat([rcs_data_sim,curr_df],axis=1)
 
@@ -148,6 +167,16 @@ def main(sim_type: str = "fft",save_path: str = os.getcwd()):
             data_file_name = os.path.basename(args[i+1])
         elif args[i] == "-settings":
             rcs_settings = loadData(args[i+1],type="settings")
+        elif args[i] == "-fs":
+            fs = int(args[i+1])
+        elif args[i] == "-nfft":
+            nfft = int(args[i+1])                # Can be 64, 256, or 1024
+        elif args[i] == "-fft_int":
+            fft_int = int(args[i+1])
+        elif args[i] == "-fft_bitshift":
+            fft_bitshift = int(args[i+1])
+        elif args[i] == "-overlap":
+            overlap_percent = float(args[i+1])
         elif args[i] == "-sim_type":
             sim_type = args[i+1]
         elif args[i] == "-save_path":
@@ -158,19 +187,49 @@ def main(sim_type: str = "fft",save_path: str = os.getcwd()):
     print(args[1])          # should be RCS settings path
     print('sucessfullly loaded data')   
 
-    # generate save name
-    if sim_type == "fft":
-        save_name = save_path + "/" + data_file_name[0:len(data_file_name)-4] + "_full_spec.csv"
-    else:
-        save_name = save_path + "/" + data_file_name[0:len(data_file_name)-4] + "_pb.csv"
+    # default values
+    if 'fs' not in locals():
+        fs = 500
 
-    # Check what type of simulation to run and save output
-    if sim_type == "fft":
-        out_DF = calcRCSFFT(rcs_data,rcs_settings)
-        out_DF.to_csv(save_name,index = False)
-    elif sim_type == "pb":
-        out_DF = calcRCSFFT(rcs_data,rcs_settings,pb = True)
-        out_DF.to_csv(save_name,index = False)
+    if 'nfft' not in locals():
+        nfft = (64,256,1024)
+
+    if 'fft_bitshift' not in locals():
+        fft_bitshift = (0,1,2,3,4,5,6,7)
+
+    if 'overlap_percent' not in locals():
+        overlap_percent = (0.5,0.6,0.7,0.8,0.9)
+
+    # Create a list of all possible combos of nfft, bitshift, and overlap percentage
+    power_sim_param_combos = list(itertools.product(overlap_percent,nfft,fft_bitshift))
+
+    # Calculate fft interval if not defined
+    fft_int_calc = lambda x,y: ((1-x)*y*1000)/500
+
+
+    # Loop through parameter combos
+    for combo in power_sim_param_combos:
+        # Get fft interval for a specific overlap percentage
+        fft_int = int(np.round(fft_int_calc(combo[0],combo[1])))
+        
+        if fft_int > 150 and fft_int < 500:
+            # generate save name
+            sim_info = "_fs_%d_nfft_%d_fftbitshift_%d_overlap_%d_fftint_%d" % (fs,combo[1],combo[2],combo[0]*100,fft_int)
+            if sim_type == "fft":
+                save_name = save_path + "/" + data_file_name[0:len(data_file_name)-4] + sim_info + "_full_spec.csv"
+            else:
+                save_name = save_path + "/" + data_file_name[0:len(data_file_name)-4] + sim_info + "_pb.csv"
+
+
+            # Check what type of simulation to run and save output
+            if sim_type == "fft":
+                out_DF = calcRCSFFT(rcs_data,rcs_settings,False,fs,combo[1],fft_int,combo[2])
+                out_DF.to_csv(save_name,index = False)
+                print("Wrote out file: %s" % save_name)
+            elif sim_type == "pb":
+                out_DF = calcRCSFFT(rcs_data,rcs_settings,True,fs,combo[1],fft_int,combo[2])
+                out_DF.to_csv(save_name,index = False)
+                print("Wrote out file: %s" % save_name)
 
 
 
